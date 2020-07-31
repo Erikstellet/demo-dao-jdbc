a7e2a41ba7a0a378ed45baa3ff6b9cbb6435dcf1
Erik Stellet <erikstellet@gmail.com>
2020-07-31 08:22:29 -0300

    git commit -m

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program 
@@ -10,9 +13,18 @@ public class Program
 	{	
 		SellerDao sellerDao =  DaoFactory.createSellerDao();
 		
-		System.out.println("==================================== Test 1 - SellerFindById  ==================================== \n");
+		System.out.println("==================================== Test 1 - SellerFindById  ===================================== \n");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		System.out.println("");
+		
+		System.out.println("================================= Test 2 - SellerFindByDepartment  ================================= \n");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj: list)
+		{
+			System.out.println(obj);
+			System.out.println("");
+		}
 	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -81,8 +81,11 @@ public class Seller implements Serializable
 	@Override
 	public String toString() 
 	{
-		return "Seller id: " + id + ", name: " + name + ", email: " 
-				+ email + ", birthDate: " + birthDate + ", baseSalary: " 
-				+ baseSalary + "\ndepartment: " + department ;
+		return "Seller id: " + id 
+				+ ", name: " + name 
+				+ ", email: " + email 
+				+ ", birthDate: " + birthDate 
+				+ ", baseSalary: " + baseSalary 
+				+ "\nDepartment: " + department ;
 	}
 }
